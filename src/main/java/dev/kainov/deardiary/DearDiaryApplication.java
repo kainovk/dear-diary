742a8af9b99c21d8713ad6f1bf8c56595b807517
Kirill Kainov <kaynov.w@gmail.com>
2022-04-04 15:21:45 +0300

    init

---
+++ src/main/java/dev/kainov/deardiary/DearDiaryApplication.java
@@ -0,0 +1,13 @@
+package dev.kainov.deardiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DearDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DearDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kainov/deardiary/DearDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kainov.deardiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
