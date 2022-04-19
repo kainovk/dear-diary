b858e6f08aef9a73b349f2cf738edce0309af28e
Kirill Kainov <kaynov.w@gmail.com>
2022-04-19 20:27:18 +0300

    set up basic auth

---
+++ src/main/java/dev/kainov/deardiary/security/config/WebSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package dev.kainov.deardiary.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.httpBasic().and().authorizeRequests()
+                .antMatchers("/api/v*/notes").hasRole("ADMIN")
+                .antMatchers("/api/v*/users").permitAll()
+                .and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("userpass").roles("USER").and()
+                .withUser("admin").password("adminpass").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
